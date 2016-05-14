c6d74ecc38b0c74ace6fe040d490999451687fc0
htinlinn <linn@prolificinteractive.com>
2016-05-14 09:08:44 -0400

    Updated the project and Pod files

---
+++ Example/Pods/Target Support Files/Optik/Optik-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double OptikVersionNumber;
+FOUNDATION_EXPORT const unsigned char OptikVersionString[];
+
---
+++ Example/Pods/Target Support Files/Pods-Optik_Tests/Pods-Optik_Tests-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_Optik_TestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_Optik_TestsVersionString[];
+
